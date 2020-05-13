a7694b0b3a84865c42f719ba9b37a9c2737bc87d
Kyle Murray <kymurray@microsoft.com>
2020-05-13 13:40:25 -0700

    clean up

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -34,139 +34,141 @@ object App {
   def main(args:Array[String]): Unit = {
     Console.println("Hello World!")
 
-    val asdf = betterLowerRemoveAllWhitespace("ASDF ASDF")
-    println(asdf)
-    println(asdf.getOrElse())
-    val asdf2 = betterLowerRemoveAllWhitespace(null)
-    println(asdf2.getOrElse())
-    println(None)
+    // TODO uncomment each section as you go
 
-    val dots = "a1.b2.c3.d4"
-    println(tryGetNth(dots, 3))
-    tryGetNth(dots, 5) match {
-      case Success(i) => println(i)
-      case Failure(e) => println(s"Failed with $e")
-    }
-
-    println(
-      optionGetNth(dots, 3)
-        .map(s => s.toUpperCase() // lambda expression
-          .concat("*"))
-        .getOrElse("index out of bounds")
-    )
-    println(
-      optionGetNth(dots, 5)
-        .map(s => s.toUpperCase()
-          .concat("*"))
-        .getOrElse("index out of bounds")
-    )
-
-    // no such thing as operators
-    val a : Int = 5
-    println(a + 2)
-    val num : WeirdNum = new WeirdNum(6);
-    println(num + 2)
-    println(num %^&* 2)
-    println(num times 2) // wouldn't actually use methods like this
-
-    // implicits
-    val num2 : WeirdNum = 5;
-    println(num2 + 2)
-
- // DATAFRAMES
- // https://www.youtube.com/watch?v=gj9g8QSyssQ
-    import spark.implicits._
-    val sampleDf = Seq(
-      (10, "cat", "new york"),
-      (4, "dog", "durham"),
-      (7, null, "new delhi")
-    ).toDF("num", "animal", "city")
-
-    sampleDf
-      .withColumn("city_starts_with_new", sampleDf("city").startsWith("new"))
-      .withColumn("less than 5", col("num").lt(5))
-      .withColumn("num plus five", col("num") + 5) // remember this is .+(5), calling the col method
-      //.withColumn("num plus five", 5 + col("num")) // need to convert to lit(5)
-      .withColumn("animal first two upper", upper(col("animal").substr(0, 2)))
-      .show()
-
-    sampleDf
-      .filter(col("animal").isNotNull)
-      .show()
-
-    sampleDf
-      .withColumn(
-        "word_comparison",
-        when(col("animal").substr(0, 1) === col("city").substr(0, 1), "starts with same")
-          .when(lit(true), "override") // else if
-          .otherwise("nope")
-      ).show()
-
-    // note could read as json, but this will give us some practice
-    var df = spark.read.csv("src\\test\\resources\\Data\\audit-data.txt")
-    var origDf = df
-    df.printSchema()
-    df.show(5)
-    val dfArray = df.collect() // Note just for testing purposes. This is a super expensive way to do this
-    for(i <- 1 to 5){
-      println("row" + i + ": " + dfArray(i))
-      // todo select column to print
-      println("first column of row" + i + ": " + dfArray(i).toSeq(0))
-      println("second column of row" + i + ": " + dfArray(i).toSeq(1))
-    }
-
-    // todo add computed column
-    var newDf = df.withColumn("_c0-copy", col("_c0"))
-    newDf.printSchema()
-    println("New column contents: " + newDf.collect().last.toSeq.last)
-
-    // todo extract column contents (ask for ideas? need udf)
-    println("\n-----------------------------------------------------------\n")
-
-    newDf = df.withColumn("CreationTime", getColumnContentsUdf(col("_c0")))
-    println("New column name: " + newDf.schema.last.name)
-    newDf.show(5)
-    println("New column contents: " + newDf.collect().last.toSeq.last) // Note just for testing purposes. This is a super expensive way to do this
-
-    // todo foreach extraction for all columns
-    newDf = df
-        .withColumn("UserId", getColumnContentsUdf(col("_c11")))
-        .withColumn("Operation", getColumnContentsUdf(col("_c2")))
-        .withColumn("ItemType", parseContents2(col("_c14"))) // better if possible
-        .select("UserId", "Operation", "ItemType")
-    newDf.printSchema()
-    newDf.collect().foreach(println)
-
-    // todo enrich
-    val recipientInfoLookupTable = spark.read
-      .json("src\\test\\resources\\Data\\sample-recipient-lookup-table.txt")
-    recipientInfoLookupTable.show()
-
-    //var userInfo = spark.read.text("src\\test\\resources\\Data\\sample-recipient-lookup-table.txt")
-    var joinedInfo = newDf.join(recipientInfoLookupTable, col("UserId") === recipientInfoLookupTable("Upn"),
-    "left_outer")
-    joinedInfo.show(15)
-
-    // todo filter
-    var filteredInfo = joinedInfo.filter(joinedInfo("Name") !== "null")
-    filteredInfo.show(5)
-
-    // todo agg
-    filteredInfo
-      .groupBy("Name")
-      .agg(count("Operation") as "count")
-      .orderBy("count")
-      .show()
-
-    filteredInfo
-      .filter(filteredInfo("Name") === "John Dow")
-      .groupBy("Name", "Operation")
-      .agg(count("Operation") as "count")
-      .orderBy(desc("count"))
-      .show()
-
-    // todo flatten and map https://www.geeksforgeeks.org/scala-flatmap-method/
-    // todo options, let's say the split fails sometimes and I want it to just ignore the column in that case
+//    // no such thing as operators
+//    val a : Int = 5
+//    println(a + 2)
+//    val num : WeirdNum = new WeirdNum(6);
+//    println(num + 2)
+//    println(num %^&* 2)
+//    println(num times 2) // wouldn't actually use methods like this
+//
+//    // implicits
+//    val num2 : WeirdNum = 5;
+//    println(num2 + 2)
+//
+//    // option / try
+//    val str = "a1.b2.c3.d4"
+//    println(getNth(str, 3))
+//
+//    println(tryGetNth(str, 3))
+//    tryGetNth(str, 5) match {
+//      case Success(i) => println(i)
+//      case Failure(e) => println(s"Failed with $e")
+//    }
+//
+//    var result = optionGetNth(str, 4) // returns d4
+//      .map(entry => entry.toUpperCase()) // returns D4
+//      .filter(entry => entry.contains('4')) // returns D4
+//      .map(entry => entry + '$') // returns D4$
+//      .map(entry => entry.map(e => e.equals('D'))) // returns true false false
+//      .getOrElse("failed")
+//
+//    result = tryGetNth(str, 4) // returns d4
+//      .map(entry => entry.toUpperCase()) // returns D4
+//      .filter(entry => entry.contains('4')) // returns D4
+//      .map(entry => entry + '$') // returns D4$
+//      .map(entry => entry.map(e => e.equals('D'))) // returns true false false
+//
+//    println(result)
+//
+//
+// // DATAFRAMES
+// // https://www.youtube.com/watch?v=gj9g8QSyssQ
+//    import spark.implicits._
+//    val sampleDf = Seq(
+//      (10, "cat", "new york"),
+//      (4, "dog", "durham"),
+//      (7, null, "new delhi")
+//    ).toDF("num", "animal", "city")
+//
+//    sampleDf
+//      .withColumn("city_starts_with_new", sampleDf("city").startsWith("new"))
+//      .withColumn("less than 5", col("num").lt(5))
+//      .withColumn("num plus five", col("num") + 5) // remember this is .+(5), calling the col method
+//      //.withColumn("num plus five", 5 + col("num")) // need to convert to lit(5)
+//      .withColumn("animal first two upper", upper(col("animal").substr(0, 2)))
+//      .show()
+//
+//    sampleDf
+//      .filter(col("animal").isNotNull)
+//      .show()
+//
+//    sampleDf
+//      .withColumn(
+//        "word_comparison",
+//        when(col("animal").substr(0, 1) === col("city").substr(0, 1), "starts with same")
+//          .when(lit(true), "override") // else if
+//          .otherwise("nope")
+//      ).show()
+//
+//    // note could read as json, but this will give us some practice
+    // todo load dataframe from file and show contents
+//    var df = spark.read.csv("src\\test\\resources\\Data\\audit-data.txt")
+//    var origDf = df
+//    df.printSchema()
+//    df.show(5)
+//    val dfArray = df.collect() // Note just for testing purposes. This is a super expensive way to do this
+//    for(i <- 1 to 5){ // for loop is cheating. scala is not purely functional
+//      println("row" + i + ": " + dfArray(i))
+//      // todo select column to print
+//      println("first column of row" + i + ": " + dfArray(i).toSeq(0))
+//      println("second column of row" + i + ": " + dfArray(i).toSeq(1))
+//    }
+//
+//    // todo add computed (copied) column
+//    var newDf = df.withColumn("_c0-copy", col("_c0"))
+//    newDf.printSchema()
+//    println("New column contents: " + newDf.collect().last.toSeq.last)
+//
+//    // todo extract column contents
+//    println("\n-----------------------------------------------------------\n")
+//
+//    newDf = df.withColumn("CreationTime", getColumnContentsUdf(col("_c0")))
+//    println("New column name: " + newDf.schema.last.name)
+//    newDf.show(5)
+//    println("New column contents: " + newDf.collect().last.toSeq.last) // Note just for testing purposes. This is a super expensive way to do this
+//
+//    // todo extraction for other columns
+//    newDf = df
+//        .withColumn("UserId", getColumnContentsUdf(col("_c11")))
+//        .withColumn("Operation", getColumnContentsUdf(col("_c2")))
+//        .withColumn("ItemType", parseContents2(col("_c14"))) // better if possible
+//        .select("UserId", "Operation", "ItemType")
+//    newDf.printSchema()
+//    newDf.collect().foreach(println)
+//
+//    // todo enrich
+//    val recipientInfoLookupTable = spark.read
+//      .json("src\\test\\resources\\Data\\sample-recipient-lookup-table.txt")
+//    recipientInfoLookupTable.show()
+//
+//    //var userInfo = spark.read.text("src\\test\\resources\\Data\\sample-recipient-lookup-table.txt")
+//    var joinedInfo = newDf.join(recipientInfoLookupTable, col("UserId") === recipientInfoLookupTable("Upn"),
+//    "left_outer")
+//    joinedInfo.show(15)
+//
+//    // todo filter
+//    var filteredInfo = joinedInfo.filter(joinedInfo("Name") !== "null")
+//    filteredInfo.show(5)
+//
+//    // todo agg
+//    filteredInfo
+//      .groupBy("Name")
+//      .agg(count("Operation") as "count")
+//      .orderBy("count")
+//      .show()
+//
+//    filteredInfo
+//      .filter(filteredInfo("Name") === "John Dow")
+//      .groupBy("Name", "Operation")
+//      .agg(count("Operation") as "count")
+//      .orderBy(desc("count"))
+//      .show()
+//
+//    // todo flatten and map https://www.geeksforgeeks.org/scala-flatmap-method/
+//    // todo options, let's say the split fails sometimes and I want it to just ignore the column in that case
   }
 
 //  val getColumnContentsUdf: UserDefinedFunction = udf(parseContents _)
@@ -191,9 +193,15 @@ object App {
     }
   }
 
+  val lowerRemoveWhiteUdf = udf[Option[String], String](betterLowerRemoveAllWhitespace)
+
   def betterLowerRemoveAllWhitespace(s: String): Option[String] = {
     val str = Option(s).getOrElse(return None) // roundabout null check
     Some(str.toLowerCase().replaceAll("\\s", ""))
+  }
+
+  def getNth(s: String, n: Int)  {
+    s.split('.')(n - 1)
   }
 
   def tryGetNth(s: String, n: Int) : Try[String] = {
@@ -203,7 +211,7 @@ object App {
   def optionGetNth(s: String, index: Int) : Option[String] = {
     val split = s.split('.')
     if (index < split.length) {
-      Some(split(index))
+      Some(split(index - 1))
     }
     else {
       None
